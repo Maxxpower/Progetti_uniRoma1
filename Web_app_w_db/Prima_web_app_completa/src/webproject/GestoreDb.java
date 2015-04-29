8c1f3f5102d2c9e2de6333664f9d0e769fc9ffa3
Maxxpower <Maxxpower@192.168.0.184>
2015-04-29 10:23:12 +0200

    commit 29/04

--- Web_app_w_db/Prima_web_app_completa/src/webproject/GestoreDb.java
+++ Web_app_w_db/Prima_web_app_completa/src/webproject/GestoreDb.java
@@ -1,5 +1,91 @@
 package webproject;
-
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+ 
 public class GestoreDb {
+ 
+        //questa classe si occuperà di garantire le operazioni più utili per interfacciarsi con la nostra tabella utenti
+       
+        private String protocol="jdbc:derby:";
+        private String dbname="primoDb";
+        private String dbURL=protocol+"//localhost"+dbname;
+       
+        static final String username="user";
+        static final String password="password";
+        
+        //ricerca per username, restituisce in un'arraylist utente e password
+        
+        public ArrayList<String> findByName(String name){
+        		
+        		Connection c=null;
+        		Statement s=null;
+        		PreparedStatement psquery=null;
+        		ResultSet rs=null;
+        	
+                ArrayList<String> result=new ArrayList<String>();
+               
+                try {
+                        c=DriverManager.getConnection(dbURL, username, password);
+                        s=c.createStatement();
+                        psquery=c.prepareStatement("select username,pwd from users where username=?");
+                        psquery.setString(1,name);
+                        psquery.executeUpdate();
+                        
+                        
+                        
+                        
+                        rs=psquery.getResultSet();
+                        while(rs.next()){
+                               
+                                result.add(rs.getString("username"));
+                                result.add(rs.getString("pwd"));
+                               
+                               
+                        }
+                       
+                } catch (SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }finally{
+                	
+                	
+                	try {
+						rs.close();
+	                	s.close();
+	                	psquery.close();
+	                	c.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 
+                	
+                	
+                	
+                	
+                	
+                }
+               
+               
+               
+                return result;
+               
+               
+               
+               
+               
+        }
+       
+       
+       
+       
+       
+       
+       
 }
--- Web_app_w_db/Prima_web_app_completa/src/webproject/LoginServlet.java
+++ Web_app_w_db/Prima_web_app_completa/src/webproject/LoginServlet.java
@@ -1,16 +1,20 @@
 package webproject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class DbAccessServlet
  */
-@WebServlet("/DbAccessServlet")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,14 +30,46 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		//leggiamo username e password dalla richiesta per poi confrontarli con quelli presenti sul db
+		
+		String user=request.getParameter("username");
+		String pwd=request.getParameter("password");
+		HttpSession session=request.getSession();
+		
+		//nuovo oggetto gestoreDB
+		GestoreDb g= new GestoreDb();
+		ArrayList<String> founded=g.findByName(user);
+		
+		if(user.equals(founded.get(0)) && pwd.equals(founded.get(1))){
+			
+			session.setAttribute("username", user);
+			RequestDispatcher rs=request.getRequestDispatcher("/WEB-INF/logged.jsp");
+			rs.forward(request, response);
+			
+			
+		}else{
+			
+			RequestDispatcher rs=request.getRequestDispatcher("index.jsp");
+			rs.forward(request, response);
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
