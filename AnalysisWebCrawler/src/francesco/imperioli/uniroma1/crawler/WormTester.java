d221d326fa3f7d5d4d378eb880fd8db8d43b1acf
Francesco <Francesco@192.168.1.85>
2016-10-17 23:37:45 +0200

    new project

---
+++ AnalysisWebCrawler/src/francesco/imperioli/uniroma1/crawler/WormTester.java
@@ -0,0 +1,19 @@
+package francesco.imperioli.uniroma1.crawler;
+
+
+
+public class WormTester {
+
+	public static void main(String[] args) {
+	
+		WormCrawler wc= new WormCrawler();
+		wc.ricerca("http://www.eprice.it", "[src]");
+		
+		
+		
+		
+		
+
+	}
+
+}
