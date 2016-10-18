2fe9c9c8952527574f0f6e8f8bf2a7fbd11e8eaa
Maxxpower <maxxpower992@live.it>
2016-10-18 19:23:07 +0200

    ho aggiunto la richiesta del dominio al funzione getcookies

--- AnalysisWebCrawler/src/francesco/imperioli/uniroma1/crawler/WormTester.java
+++ AnalysisWebCrawler/src/francesco/imperioli/uniroma1/crawler/WormTester.java
@@ -1,13 +1,16 @@
 package francesco.imperioli.uniroma1.crawler;
 
-
+import test.francesco.imperioli.uniroma1.crawler.CookiesFinder;
 
 public class WormTester {
 
 	public static void main(String[] args) {
 	
-		WormCrawler wc= new WormCrawler();
-		wc.ricerca("http://www.myprotein.com", "[src]");
+		//WormCrawler wc= new WormCrawler();
+		//wc.ricerca("http://huffingtonpost.com", "[src]");
+		CookiesFinder.findCookiesByUrl("http://www.myprotein.com");
 	}
+	
+		
 
 }
