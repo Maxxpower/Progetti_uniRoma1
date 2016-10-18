6da93052ca88f07d97fad89e98391473aa5a0638
fabioimperioli <fabio.imperioli@gmail.com>
2016-10-18 16:16:20 +0200

    prova su myprotein

--- AnalysisWebCrawler/src/francesco/imperioli/uniroma1/crawler/WormTester.java
+++ AnalysisWebCrawler/src/francesco/imperioli/uniroma1/crawler/WormTester.java
@@ -7,7 +7,7 @@ public class WormTester {
 	public static void main(String[] args) {
 	
 		WormCrawler wc= new WormCrawler();
-		wc.ricerca("http://www.eprice.it", "[src]");
+		wc.ricerca("http://www.myprotein.com", "[src]");
 	}
 
 }
